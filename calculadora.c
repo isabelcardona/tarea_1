68bc33a32a4f219799923c89d13ac08b93030b88
isabelcardona <iccardonas@uqvirtual.edo.co>
2015-08-13 12:18:27 -0500

    edicion del archivo calculadora.c

---
+++ calculadora.c
@@ -0,0 +1,17 @@
+#include "calculadora.h"
+int suma(a,b)
+{
+    return a+b;
+}
+int resta(a,b)
+{
+    return a-b;
+}
+int multiplicacion(a,b)
+{
+    return a*b;
+}
+int division(a,b)
+{
+    return a/b;
+}
