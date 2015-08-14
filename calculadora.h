9eabaf8c74802054e00c865501fea67914033702
isabelcardona <iccardonas@uqvirtual.edu.co>
2015-08-14 17:40:00 -0500

    se ha documentado el archivo calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -1,4 +1,32 @@
+
 int suma (int x, int y);
+/**
+*\brief funcion que suma
+*\param x operando 1
+*\param y operando 2
+*\return un entero con el resultado 
+*/
+
 int resta  (int x, int y);
+/**
+*\brief funcion que resta
+*\param x operando 1
+*\param y operando 2
+*\return un entero con el resultado 
+*/
+
 int division  (int x, int y);
+/**
+*\brief funcion que divide
+*\param x operando 1
+*\param y operando 2
+*\return un entero con el resultado 
+*/
+
 int multiplicacion  (int x, int y);
+/**
+*\brief funcion que multiplica
+*\param x operando 1
+*\param y operando 2
+*\return un entero con el resultado 
+*/
